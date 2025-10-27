2272f4297005e8689cb552473ebbad96bd438e87
HwGEscolar <christofer.vecchia.fernandes@escola.pr.gov.br>
2025-10-27 11:26:32 -0300

    ghhhg

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const avanca = document;querySelectorAll(".btn-proximo")
 console.log(avanca)
+
+avanca.onbeforematch( button=>{
+    button.addEventListener('click', function(){
+    const atual = document.querySelector('.ativo')
+    const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
+
+    atual.classList.remove('.ativo')
+    document.getElementById(proximoPasso).classLito.addd('ati')
+})
+})
